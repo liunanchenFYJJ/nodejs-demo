04e0c3b7fe06378bb8524b7af27680d02df9302c
liunanchenFYJJ <liunan_chen@163.com>
2018-01-10 10:31:26 +0800

    create an lite package(somepackage)

---
+++ nodejsdemo/getsomepackage.js
@@ -0,0 +1,8 @@
+/**
+ * Created by https://github.com/liunanchenFYJJ on 2018/1/10.
+ */
+
+// 使用建立的somepackage包
+var somepackage = require('./somepackage')
+
+somepackage.hello()
---
+++ nodejsdemo/somepackage/lib/interface.js
@@ -0,0 +1,8 @@
+/**
+ * Created by https://github.com/liunanchenFYJJ on 2018/1/10.
+ */
+
+// 制作一个最简单的示意包
+exports.hello = function () {
+    console.log('hello there')
+}
